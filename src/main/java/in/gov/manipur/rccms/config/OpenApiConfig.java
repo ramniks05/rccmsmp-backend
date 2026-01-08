2c90a2943141827de8954727e19b38b72145640d
Ramesh kumar <ramsitservices05@gmail.com>
2026-01-08 15:48:23 +0530

    feat: Add JWT Bearer token authentication to Swagger UI
    - Add JWT Bearer token security scheme to OpenAPI configuration
    - Enable Authorize button in Swagger UI for token input
    - Configure security requirement for all endpoints
    - Users can now set JWT token in Swagger header for API testing
    - Token will be automatically included in Authorization header

--- src/main/java/in/gov/manipur/rccms/config/OpenApiConfig.java
+++ src/main/java/in/gov/manipur/rccms/config/OpenApiConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,9 +59,23 @@ public class OpenApiConfig {
                         """)
                 .license(license);
 
+        // JWT Bearer Token Security Scheme
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Enter JWT token obtained from login endpoint");
+
+        // Security Requirement
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(devServer, prodServer));
+                .servers(List.of(devServer, prodServer))
+                .addSecurityItem(securityRequirement)
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", bearerAuth));
     }
 }
 
