5bb47e159cc63fdcdc57d0190afd0abafe8bdc84
satpal <satpal@satpals-MacBook-Air.local>
2026-02-03 17:58:13 +0530

    merge with mian

--- src/main/java/in/gov/manipur/rccms/RccmsmpBackendApplication.java
+++ src/main/java/in/gov/manipur/rccms/RccmsmpBackendApplication.java
@@ -3,7 +3,6 @@ package in.gov.manipur.rccms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -22,7 +21,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EntityScan(basePackages = "in.gov.manipur.rccms.entity")
 @EnableJpaRepositories(basePackages = "in.gov.manipur.rccms.repository")
-@EnableDiscoveryClient
 public class RccmsmpBackendApplication {
 
     public static void main(String[] args) {
